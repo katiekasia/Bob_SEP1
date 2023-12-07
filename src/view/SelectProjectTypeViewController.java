40973b4e01ac827ed5c98f3f0a3009fdb556b963
katiekasia <olejarczykkatarzyna2022@gmail.com>
2023-12-07 14:43:25 +0100

    back and cancel button for Select Type

--- src/view/SelectProjectTypeViewController.java
+++ src/view/SelectProjectTypeViewController.java
@@ -67,12 +67,20 @@ public class SelectProjectTypeViewController {
     viewHandler.openView("commercialProject"); // Call the method to open create Commercial Project
   }
   @FXML
-  private void handleIndustrialButton(ActionEvent event) {
+  private void handleIndustrialButton() {
     viewHandler.openView("industrialProject");
   }
   @FXML
-  private void handleRoadConstructionButton(ActionEvent event) {
+  private void handleRoadConstructionButton() {
     viewHandler.openView("roadConstructionProject");
+  }
+  @FXML
+  private void handleCancelButton() {
+    viewHandler.openView("projects");
+  }
+  @FXML
+  private void handleBackButton() {
+    viewHandler.openView("projects");
   }
 
 
