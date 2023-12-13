28ee6bc24a19a03b13552026d8a867e086049a8e
SebinoBR <sbrbartko@gmail.com>
2023-12-13 13:20:08 +0100

    push psuhsh
    COOL PROJECTS COOL COMMENTS FOR FIRST CLASS

--- src/model/Commercial.java
+++ src/model/Commercial.java
@@ -1,54 +1,75 @@
-package model;
+package model; // a comment describing the package or its purpose, currently commented out.
 
+// class representing a commercial project, extending the Project class
 public class Commercial extends Project
 {
-  private int numberOfFloors;
-  private int timeline;
-  private String useOfBuilding;
+  // private instance variables for commercial projects
+  private int numberOfFloors; // number of floors in the building
+  private int timeline; // project timeline in days
+  private String useOfBuilding; // purpose or use of the building, e.g., office, retail, etc.
 
-
-//  the same thing for default settings as for Residential
-  public Commercial(int ID, String title, double budget, double size, String address, ProjectType type,int numberOfFloors,int timeline, String useOfBuilding)
+  // constructor for creating commercial objects
+  // parameters: ID - project ID, title - project title, budget - project budget, size - project size,
+  // address - project address, type - project type, numberOfFloors - number of floors in the building,
+  // timeline - project timeline, useOfBuilding - purpose or use of the building
+  public Commercial(int ID, String title, double budget, double size, String address, ProjectType type,
+      int numberOfFloors, int timeline, String useOfBuilding)
   {
-    super(ID,title,budget,size,address,type);
+    // call the constructor of the superclass (Project) with specified parameters
+    super(ID, title, budget, size, address, type);
 
+    // load default commercial settings using DefaultSettingsHandler
     Object[] defaultCommercial = DefaultSettingsHandler.loadCommercialDefaultSettings();
+
+    // set numberOfFloors to the provided value if not 0, otherwise set it to the default value
     this.numberOfFloors = (numberOfFloors != 0) ? numberOfFloors : (int) defaultCommercial[0];
+
+    // set timeline to the provided value if not 0, otherwise set it to the default value
     this.timeline = (timeline != 0) ? timeline : (int) defaultCommercial[1];
+
+    // set useOfBuilding to the provided value
     this.useOfBuilding = useOfBuilding;
   }
 
+  // getter method for numberOfFloors
   public int getNumberOfFloors()
   {
     return numberOfFloors;
   }
 
+  // setter method for numberOfFloors
   public void setNumberOfFloors(int numberOfFloors)
   {
     this.numberOfFloors = numberOfFloors;
   }
 
+  // getter method for timeline
   public int getTimeline()
   {
     return timeline;
   }
 
+  // setter method for timeline
   public void setTimeline(int timeline)
   {
     this.timeline = timeline;
   }
 
+  // getter method for useOfBuilding
   public String getUseOfBuilding()
   {
     return useOfBuilding;
   }
 
+  // setter method for useOfBuilding
   public void setUseOfBuilding(String useOfBuilding)
   {
     this.useOfBuilding = useOfBuilding;
   }
 
+  // override toString method to provide a human-readable string representation of the commercial object
   public String toString() {
+    // return a formatted string with details about the commercial object
     return "Commercial{" +
         "ID=" + getID() +
         ", title='" + getTitle() + '\'' +
